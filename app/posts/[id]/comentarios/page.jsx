6efe37a979ebe0afce4a786bce6de988d20c3c67
Lilia Padilla <lilopadilla22@gmail.com>
2023-09-10 12:59:09 -0500

    add page content comments

--- app/posts/[id]/comentarios/page.jsx
+++ app/posts/[id]/comentarios/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
 
-export default function page() {
-  return (
-    <div>
-      <p>SERE UN COMENTARIO</p>
-    </div>
-  )
+const fetcComments = async (id) => {
+    await new Promise(resolve => setTimeout(resolve, 3000))
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    .then(res => res.json())
+}
+
+export default async function Post({params}) {
+
+    const { id } = params
+    const comments = await fetcComments(id)
+
+    return (
+        <ul>
+            {comments.map(comment => (
+                <li key={comment.id}>
+                    <p className="text-sm bg-gray-100">{comment.body}</p>
+                </li>
+            ))}
+        </ul>
+    )
 }
