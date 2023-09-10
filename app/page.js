49776f472ddee346c53cdf9ac026deb5ab1db5a0
Lilia Padilla <lilopadilla22@gmail.com>
2023-09-10 11:22:52 -0500

    update page home

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 export default function HomePage() {
+  
   return (
-    <h1 className="text-3xl font-bold underline">page</h1 >
+    <h1 className="text-3xl font-bold underline">
+      Bienvenidos a nuestra pagina de Post!
+    </h1 >
   )
 }
