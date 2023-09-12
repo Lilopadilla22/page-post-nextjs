b23c1eac40dba91ffd54706a07146da5db3b30ae
Lilia Padilla <lilopadilla22@gmail.com>
2023-09-12 14:37:56 -0500

    add loading in comments

---
+++ app/posts/[id]/comentarios/loading.jsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+  return (
+    <div>
+      <p> Cargando comentarios ...</p>
+    </div>
+  )
+}
--- app/posts/[id]/comentarios/page.jsx
+++ app/posts/[id]/comentarios/page.jsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react"
+import Loading from "./loading"
 
 const fetcComments = async (id) => {
     await new Promise(resolve => setTimeout(resolve, 3000))
@@ -11,13 +13,15 @@ export default async function Post({params}) {
     const comments = await fetcComments(id)
 
     return (
-        <ul className="mt-6">
-            {comments.map(comment => (
-                <li className="text-sm bg-gray-100 mt-2" key={comment.id}>
-                    <h2 className="text-sky-400">{comment.name}</h2>
-                    <p>{comment.body}</p>
-                </li>
-            ))}
-        </ul>
+        <Suspense fallback={<Loading/>}>
+            <ul className="mt-6">
+                {comments.map(comment => (
+                    <li className="text-sm bg-gray-100 mt-2" key={comment.id}>
+                        <h2 className="text-sky-400">{comment.name}</h2>
+                        <p>{comment.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </Suspense>
     )
 }
